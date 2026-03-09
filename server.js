c9b1ae74aa36bb08bf00fe75d0d4dc89f372334f
Samson <sams1307wolde@gmail.com>
2026-03-09 11:31:41 +0300

    Add more test routes

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const port = process.env.PORT || 7002;
 
 const app = express();
 
+app.get("/test-1", (req, res) => {
+  return res.send("Welcome to iFlow!");
+});
+app.get("/test-2", (req, res) => {
+  return res.json({ status: "ok", message: "Welcome to iFlow!" });
+});
 app.get("/", (req, res) => {
-  res.json({ status: "ok", message: "Welcome to iFlow!" });
+  return res.json({ status: "ok", message: "Welcome to iFlow!" });
 });
 
-// Deployment
+// // Deployment
 module.exports = serverless(app);
 
-// Development
+// // Development
 // app.listen(port, () => {
 //   `Server started on port ${port}!`;
 //   console.log(`Server started on port ${port}`);
