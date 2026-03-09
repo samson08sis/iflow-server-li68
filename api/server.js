9f2f7c350e704b66d413141b9caf1fbc472e6d3e
Samson <sams1307wolde@gmail.com>
2026-03-09 13:30:54 +0300

    Fix DB connection call

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 require("dotenv").config();
 
+const connectDB = require("../config/db");
 const express = require("express");
 const routes = require("../routes");
 const port = process.env.PORT || 7002;
 
 const app = express();
+
+// Connect DB
+(async () => {
+  await connectDB();
+})();
 
 // All routes
 app.use("/", routes);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-const connectDB = require("../config/db");
 const express = require("express");
 const router = express.Router();
 
