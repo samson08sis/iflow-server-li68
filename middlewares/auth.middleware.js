7c86d0c64ad43b282e7faa9d3f225f8d307a5163
Samson <sams1307wolde@gmail.com>
2026-03-13 09:52:54 +0300

    Populate vehicles of a user and selected vehicle with user response in auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+
 async function authMiddleware(req, res, next) {
   try {
     const authHeader = req.headers.authorization;
@@ -8,7 +9,10 @@ async function authMiddleware(req, res, next) {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    const user = await User.findById(decoded.id);
+    const user = await User.findById(decoded.id)
+      .populate("vehicles")
+      .populate("selectedVehicle");
+
     if (!user) throw new Error("User not found");
 
     req.user = user;
