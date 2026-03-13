29353a182fc93121b1847e43c7f139686455d706
Samson <sams1307wolde@gmail.com>
2026-03-13 10:07:47 +0300

    Fix path type

--- routes/vehicle.routes.js
+++ routes/vehicle.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middleware/auth");
+const authMiddleware = require("../middlewares/auth.middleware");
 const vehicleController = require("../controllers/vehicle.controller");
 
 router.use(authMiddleware);
