46447c76c38fcd8969b2f43d043b536d1912b073
Samson <sams1307wolde@gmail.com>
2026-03-13 09:36:27 +0300

    Define authentication middleware for protected routes

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+async function authMiddleware(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) throw new Error("No token provided");
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+    if (!user) throw new Error("User not found");
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).json({ success: false, message: "Unauthorized" });
+  }
+}
+
+module.exports = authMiddleware;
