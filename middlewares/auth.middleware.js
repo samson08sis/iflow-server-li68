82b2bea806cb8049a5f152ff1cd8fd699a7e4d2c
Samson <sams1307wolde@gmail.com>
2026-03-13 10:31:37 +0300

    Update environment variable ref tor auth middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -7,7 +7,7 @@ async function authMiddleware(req, res, next) {
     if (!authHeader) throw new Error("No token provided");
 
     const token = authHeader.split(" ")[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
     const user = await User.findById(decoded.id)
       .populate("vehicles")
